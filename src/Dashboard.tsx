869999f13e6f503c0d69f6cf23ffc14385a361ea
stevenrhett <johnsonsteven18@gmail.com>
2023-01-26 20:41:05 -0500

    working on the signin page and the mobile signin page

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,6 +1,5 @@
 // @ts-ignore
-import React from "react";
-import { Fragment, useState } from "react";
+import React, { Fragment, useState } from "react";
 import { Dialog, Menu, Transition } from "@headlessui/react";
 import {
   Bars3BottomLeftIcon,
@@ -14,19 +13,22 @@ import {
   XMarkIcon,
 } from "@heroicons/react/24/outline";
 import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
+// import "./App.css";
 import Body from "./body";
+// @ts-ignore
+import coderthinking from "./assets/coderthinking.svg";
 
+// const userNavigation = [
+//   { name: "Your Profile", href: "#" },
+//   { name: "Sign out", href: "#" },
+// ];
 const sidebarNavigation = [
   { name: "Home", href: "#", icon: HomeIcon, current: true },
-  { name: "All Files", href: "#", icon: Squares2X2Icon, current: false },
+  { name: "My Work", href: "#", icon: Squares2X2Icon, current: false },
   { name: "Photos", href: "#", icon: PhotoIcon, current: false },
   { name: "Shared", href: "#", icon: UserGroupIcon, current: false },
   { name: "Albums", href: "#", icon: RectangleStackIcon, current: false },
   { name: "Settings", href: "#", icon: CogIcon, current: false },
-];
-const userNavigation = [
-  { name: "Your Profile", href: "#" },
-  { name: "Sign out", href: "#" },
 ];
 
 function classNames(...classes) {
@@ -40,13 +42,14 @@ export default function Dashboard() {
   // @ts-ignore
   // @ts-ignore
   // @ts-ignore
+  // @ts-ignore
   return (
     <>
       <div className="flex h-full">
         {/* Narrow sidebar */}
-        <div className="hidden w-28 overflow-y-auto bg-purple-200 md:block">
+
+        <div className="hidden w-28 overflow-y-auto bg-purple-200 md:hidden md:block">
           <div className="flex w-full flex-col items-center py-6">
-            <div className="flex flex-shrink-0 items-center"></div>
             <div className="mt-6 w-full flex-1 space-y-1 px-2">
               {sidebarNavigation.map((item) => (
                 <a
@@ -77,6 +80,7 @@ export default function Dashboard() {
         </div>
 
         {/* Mobile menu */}
+
         <Transition.Root show={mobileMenuOpen} as={Fragment}>
           <Dialog
             as="div"
@@ -130,11 +134,7 @@ export default function Dashboard() {
                     </div>
                   </Transition.Child>
                   <div className="flex flex-shrink-0 items-center px-4">
-                    {/*<img*/}
-                    {/*  className="h-8 w-auto"*/}
-                    {/*src={''}*/}
-                    {/*  alt={''}*/}
-                    {/*/>*/}
+                    <img className="h-8 w-auto" src={""} alt={""} />
                   </div>
                   <div className="mt-5 h-0 flex-1 overflow-y-auto px-2">
                     <nav className="flex h-full flex-col">
@@ -178,7 +178,7 @@ export default function Dashboard() {
         {/* Content area */}
         <div className="flex flex-1 flex-col overflow-hidden">
           <header className="w-full">
-            <div className="relative z-10 flex h-16 flex-shrink-0  bg-white shadow-sm">
+            <div className="relative z-10 flex h-16 flex-shrink-0 hover:bg-white bg-purple-50 shadow-sm">
               <button
                 type="button"
                 className=" px-4 text-purple-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-purple-500 md:hidden"
@@ -191,7 +191,7 @@ export default function Dashboard() {
                 <div className="flex flex-1">
                   <form
                     className="flex w-full bg-purple-200 md:ml-0"
-                    action="src#"
+                    action="#"
                     method="GET"
                   >
                     <label htmlFor="search-field" className="sr-only">
@@ -207,54 +207,57 @@ export default function Dashboard() {
                       <input
                         name="search-field"
                         id="search-field"
-                        className="h-full w-full border-transparent py-2 pl-8 pr-3 text-purple-900 placeholder-purple-500 focus:border-transparent focus:placeholder-purple-400 focus:outline-none focus:ring-0"
+                        className="h-full bg-purple-50 w-full hover:bg-white border-transparent py-2 pl-8 pr-3 text-purple-900 placeholder-purple-500 focus:border-transparent focus:placeholder-purple-400 focus:outline-none focus:ring-0"
                         placeholder="Search"
                         type="search"
                       />
                     </div>
                   </form>
                 </div>
-                <div className="ml-2 flex items-center space-x-4 sm:ml-6 sm:space-x-6">
+                <div className="ml-2 flex  items-center space-x-4 sm:ml-6 sm:space-x-6">
                   {/* Profile dropdown */}
-                  <Menu as="div" className="relative flex-shrink-0">
-                    <div>
-                      <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2">
-                        <span className="sr-only">Open user menu</span>
-                        {/*<img className="h-8 w-8 rounded-full" src="" alt="" />*/}
-                      </Menu.Button>
-                    </div>
-                    <Transition
-                      as={Fragment}
-                      enter="transition ease-out duration-100"
-                      enterFrom="transform opacity-0 scale-95"
-                      enterTo="transform opacity-100 scale-100"
-                      leave="transition ease-in duration-75"
-                      leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
-                    >
-                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        {userNavigation.map((item) => (
-                          <Menu.Item key={item.name}>
-                            {({ active }) => (
-                              <a
-                                href={item.href}
-                                className={classNames(
-                                  active ? "bg-purple-200" : "",
-                                  "block px-4 py-2 text-sm text-purple-900"
-                                )}
-                              >
-                                {item.name}
-                              </a>
-                            )}
-                          </Menu.Item>
-                        ))}
-                      </Menu.Items>
-                    </Transition>
-                  </Menu>
-
+                  {/*<Menu as="div" className="relative flex-shrink-0">*/}
+                  {/*  <div>*/}
+                  {/*    <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">*/}
+                  {/*      <span className="sr-only">Open user menu</span>*/}
+                  {/*      <img*/}
+                  {/*        className="h-8 w-8 rounded-full"*/}
+                  {/*        src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80"*/}
+                  {/*        alt=""*/}
+                  {/*      />*/}
+                  {/*    </Menu.Button>*/}
+                  {/*  </div>*/}
+                  {/*  <Transition*/}
+                  {/*    as={Fragment}*/}
+                  {/*    enter="transition ease-out duration-100"*/}
+                  {/*    enterFrom="transform opacity-0 scale-95"*/}
+                  {/*    enterTo="transform opacity-100 scale-100"*/}
+                  {/*    leave="transition ease-in duration-75"*/}
+                  {/*    leaveFrom="transform opacity-100 scale-100"*/}
+                  {/*    leaveTo="transform opacity-0 scale-95"*/}
+                  {/*  >*/}
+                  {/*    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">*/}
+                  {/*      {userNavigation.map((item) => (*/}
+                  {/*        <Menu.Item key={item.name}>*/}
+                  {/*          {({ active }) => (*/}
+                  {/*            <a*/}
+                  {/*              href={item.href}*/}
+                  {/*              className={classNames(*/}
+                  {/*                active ? "bg-gray-100" : "",*/}
+                  {/*                "block px-4 py-2 text-sm text-gray-700"*/}
+                  {/*              )}*/}
+                  {/*            >*/}
+                  {/*              {item.name}*/}
+                  {/*            </a>*/}
+                  {/*          )}*/}
+                  {/*        </Menu.Item>*/}
+                  {/*      ))}*/}
+                  {/*    </Menu.Items>*/}
+                  {/*  </Transition>*/}
+                  {/*</Menu>*/}
                   <button
                     type="button"
-                    className="flex items-center justify-center rounded-full bg-purple-400 p-1 text-white hover:bg-purple-500 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2"
+                    className="flex items-center  justify-center rounded-full bg-purple-400 p-1 text-white hover:bg-purple-500 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2"
                   >
                     <PlusIcon className="h-6 w-6" aria-hidden="true" />
                     <span className="sr-only">Add file</span>
@@ -263,10 +266,13 @@ export default function Dashboard() {
               </div>
             </div>
           </header>
-
           {/* Main content */}
-          <div className="flex flex-1  overflow-hidden">
+          {/*<Body />*/}
+          <div className="flex flex-1 items-stretch">
             <main className="flex-1 overflow-y-auto">
+              {/* Your content */}
+              {/* Primary column */}
+
               <section
                 aria-labelledby="primary-heading"
                 className="flex h-full min-w-0 flex-1 flex-col lg:order-last"
@@ -274,7 +280,6 @@ export default function Dashboard() {
                 <h1 id="primary-heading" className="sr-only">
                   Photos
                 </h1>
-                <Body />
               </section>
             </main>
           </div>
