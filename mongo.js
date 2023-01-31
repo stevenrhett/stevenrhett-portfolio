e092330c673fa8e586863d372dded50f47908d0e
stevenrhett <johnsonsteven18@gmail.com>
2023-01-30 22:12:16 -0500

    working on the routes and react-router-dom installation

--- mongo.js
+++ mongo.js
@@ -1,13 +1,13 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const {MongoClient, ServerApiVersion} = require("mongodb");
 const uri =
-  "mongodb+srv://admin:<bxdT2veXBIY850Tr>@cluster1.7aws5ob.mongodb.net/?retryWrites=true&w=majority";
+    "mongodb+srv://admin:<bxdT2veXBIY850Tr>@cluster1.7aws5ob.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
 });
 client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
 });
