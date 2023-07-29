eb78d503acbc90a4df56a57b472bd9c8b09d8c76
stevenrhett <johnsonsteven18@gmail.com>
2023-07-28 21:46:13 -0500

    new

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,6 +1,6 @@
-import React, { Fragment, useState } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
-import {ChevronLeftIcon, MenuIcon} from "@heroicons/react/solid";
+import React, { useState } from 'react'
+import { Dialog } from '@headlessui/react'
+import {MenuIcon} from "@heroicons/react/solid";
 
 
 const navigation = [
@@ -8,98 +8,90 @@ const navigation = [
     { name: "Projects", href: "/projects" },
     { name: "Calendar", href: "/calendar" }
 ];
-
-
-// function classNames(...classes) {
-//     return classes.filter(Boolean).join(' ')
-// }
-
-export default function Navigation() {
-    const [sidebarOpen, setSidebarOpen] = useState(false)
+    export default function Navigation() {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
     return (
-        <>
-            <div>
-                <Transition.Root show={sidebarOpen} as={Fragment}>
-                    <Dialog as="div" className="relative z-50 lg:hidden" onClose={setSidebarOpen}>
-                        <Transition.Child
-                            as={Fragment}
-                            enter="transition-opacity ease-linear duration-300"
-                            enterFrom="opacity-0"
-                            enterTo="opacity-100"
-                            leave="transition-opacity ease-linear duration-300"
-                            leaveFrom="opacity-100"
-                            leaveTo="opacity-0"
-                        >
-                            <div className="fixed inset-0 bg-gray-900/80" />
-
-
-                        </Transition.Child>
-                        <div className="fixed inset-0 flex">
-                            <Transition.Child
-                                as={Fragment}
-                                enter="transition ease-in-out duration-300 transform"
-                                enterFrom="-translate-x-full"
-                                enterTo="translate-x-0"
-                                leave="transition ease-in-out duration-300 transform"
-                                leaveFrom="translate-x-0"
-                                leaveTo="-translate-x-full"
-                            >
-                                <Dialog.Panel className="relative mr-16 flex w-full max-w-xs flex-1">
-                                    <Transition.Child
-                                        as={Fragment}
-                                        enter="ease-in-out duration-300"
-                                        enterFrom="opacity-0"
-                                        enterTo="opacity-100"
-                                        leave="ease-in-out duration-300"
-                                        leaveFrom="opacity-100"
-                                        leaveTo="opacity-0"
-                                    >
-                                        <div className="absolute left-full top-0 flex w-16 justify-center pt-5 ">
-                                            <button type="button" className="-m-2.5 p-2.5" onClick={() => setSidebarOpen(false)}>
-                                                <span className="sr-only">Close sidebar</span>
-                                                <ChevronLeftIcon className="w-6 h-6 text-white" aria-hidden="true" />
-                                            </button>
-                                        </div>
-                                    </Transition.Child>
-                                    <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl">
-                                        <div className="px-4 py-5 flex items-center justify-between border-b border-gray-200 sm:px-6">
-                                            <h6 className="text-xl font-bold text-gray-900">stevenrhett</h6>
-
-                                        </div>
-                                        <div className="flex-auto px-6 py-10 pt-0">
-                                            <nav className="flex flex-col">
-                                                {navigation.map((item) => (
-                                                    <a key={item.name} href={item.href} className=" p-3 block rounded-md text-base font-medium text-gray-900 hover:text-gray-700 hover:bg-gray-100">
-                                                        {item.name}
-                                                    </a>
-                                                ))}
-                                            </nav>
-                                        </div>
-                                    </div>
-
-                                </Dialog.Panel>
-                            </Transition.Child>
-                        </div>
-                    </Dialog>
-                </Transition.Root>
-
-                <div className="sticky top-0 left-0 z-40 flex items-center gap-x-6 bg-white py-4 shadow px-6">
-                    <button type="button" className="-m-2.5 p-2.5 text-gray-700" onClick={() => setSidebarOpen(true)}>
-                        <span className="sr-only">Open sidebar</span>
-                        <MenuIcon className="lg:hidden h-6 w-6" aria-hidden="true" />
-                    </button>
-
-                    <a href="/">
-                        <img className="h-10 w-auto" src="https://avatars.githubusercontent.com/u/10999999?v=4" alt="avatar" />
+        <header className="bg-white">
+            <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+                <div className="flex lg:flex-1">
+                    <a href="#" className="-m-1.5 p-1.5">
+                        <span className="sr-only">Your Company</span>
+                        <img className="h-8 w-auto" src="/src/assets/Ste_Rhe.svg"
+                             alt="" />
                     </a>
-
-
-
                 </div>
+                <div className="flex lg:hidden">
+                    <button
+                        type="button"
+                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                        onClick={() => setMobileMenuOpen(true)}
+                    >
+                        <span className="sr-only">Open main menu</span>
+                        <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                    </button>
+                </div>
+                <div className="hidden lg:flex lg:gap-x-12">
+                    {navigation.map((item) => (
+                        <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+                            {item.name}
+                        </a>
+                    ))}
+                </div>
+                <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                        Log in <span aria-hidden="true">&rarr;</span>
+                    </a>
+                </div>
+            </nav>
+            <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+                <div className="fixed inset-0 z-10" />
+                <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                    <div className="flex items-center justify-between">
+                        <a href="#" className="-m-1.5 p-1.5">
+                            <span className="sr-only">Your Company</span>
+                            <img
+                                className="h-8 w-auto"
+                                src="/src/assets/Ste_Rhe.svg"
+                                alt=""
+                            />
+                        </a>
+                        <button
+                            type="button"
+                            className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                            onClick={() => setMobileMenuOpen(false)}
+                        >
+                            <span className="sr-only">Close menu</span>
+                            <XMarkIcon className="h-6 w-6" aria-hidden="true" />
 
 
-            </div>
-        </>
+                        </button>
+                    </div>
+                    <div className="mt-6 flow-root">
+                        <div className="-my-6 divide-y divide-gray-500/10">
+                            <div className="space-y-2 py-6">
+                                {navigation.map((item) => (
+                                    <a
+                                        key={item.name}
+                                        href={item.href}
+                                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                    >
+                                        {item.name}
+                                    </a>
+                                ))}
+                            </div>
+                            <div className="py-6">
+                                <a
+                                    href="#"
+                                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                >
+                                    Log in
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </Dialog.Panel>
+            </Dialog>
+        </header>
     )
 }
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,36 +1,36 @@
 const products = [
     {
-      id: 1,
-      name: 'Pawsome ',
-      imageSrc: './src/assets/Screenshot 2023-04-29 at 7.57.08 PM.png',
-      href: 'https://pawsome-github-io.vercel.app/',
-      imageAlt: '',
+        id: 1,
+        name: 'Pawsome ',
+        imageSrc: './src/assets/Screenshot 2023-04-29 at 7.57.08 PM.png',
+        href: 'https://pawsome-github-io.vercel.app/',
+
     },
     {
-      id: 2,
-      name: 'Woodchuck Stump Grinding',
-      href: 'https://woodchuck-app.vercel.app/',
-      imageSrc: './src/assets/wood.png',
-      imageAlt: '',
+        id: 2,
+        name: 'Woodchuck Stump Grinding',
+        href: 'https://woodchuck-app.vercel.app/',
+        imageSrc: './src/assets/wood.png',
+
     },
     {
-      id: 3,
-      name: 'Task Tracker',
-      href: 'https://task-tracker-omega.vercel.app/',
-      imageSrc: './src/assets/Screenshot 2023-04-29 at 7.57.31 PM.png',
-      imageAlt: '',
+        id: 3,
+        name: 'Task Tracker',
+        href: 'https://task-tracker-omega.vercel.app/',
+        imageSrc: './src/assets/Screenshot 2023-04-29 at 7.57.31 PM.png',
+
     },
     {
-      id: 4,
-      name: 'Real Estate',
-      href: 'https://rainmaker82.wpcomstaging.com',
-      imageSrc: './src/assets/Screenshot 2023-07-21 at 7.50.48 AM.jpg',
-      imageAlt: '',
+        id: 4,
+        name: 'Real Estate',
+        href: 'https://rainmaker82.wpcomstaging.com',
+        imageSrc: './src/assets/Screenshot 2023-07-21 at 7.50.48 AM.jpg',
+
     },
 
-  ]
+]
 
-export default function Projects(){
+export default function Projects() {
     return (
         <div className="bg-gradient-to-r from-indigo-50 to-transparent">
             <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
@@ -39,17 +39,18 @@ export default function Projects(){
                 <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                     {products.map((product) => (
                         <div key={product.id} className="group relative">
-                            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-base-50 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                            <div
+                                className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-base-50 lg:aspect-none group-hover:opacity-75 lg:h-80">
                                 <img
                                     src={product.imageSrc}
                                     className="object-contain object-center w-full h-full"
-                                 alt=""/>
+                                    alt=""/>
                             </div>
                             <div className="mt-4 flex justify-between">
                                 <div>
                                     <h3 className="text-md text-center text-gray-700">
                                         <a href={product.href}>
-                                            <span aria-hidden="true" className="absolute inset-0" />
+                                            <span aria-hidden="true" className="absolute inset-0"/>
                                             {product.name}
                                         </a>
                                     </h3>
