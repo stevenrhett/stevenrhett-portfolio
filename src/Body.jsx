7070c3587796ddbb8721aca23f838c6701805aa4
stevenrhett <johnsonsteven18@gmail.com>
2023-07-30 17:11:38 -0400

    new

--- src/Body.jsx
+++ src/Body.jsx
@@ -5,125 +5,107 @@ export default function Body() {
 
     return (<div className="mx-2.5 p-3 mb-10">
 
-            <div className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
-                <div className="absolute inset-0 -z-10 overflow-hidden">
-                    <svg
-                        className="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]"
-                        aria-hidden="true"
-                    >
-                        <defs>
-                            <pattern
-                                id="e813992c-7d03-4cc4-a2bd-151760b470a0"
-                                width={200}
-                                height={200}
-                                x="50%"
-                                y={-1}
-                                patternUnits="userSpaceOnUse"
-                            >
-                                <path d="M100 200V.5M.5 .5H200" fill="none"/>
-                            </pattern>
-                        </defs>
-                        <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
-                            <path
-                                d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
-                                strokeWidth={0}
-                            />
-                        </svg>
-                        <rect width="100%" height="100%" strokeWidth={0}
-                              fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)"/>
-                    </svg>
-                </div>
-                <div
-                    className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
-                    <div
-                        className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
-                        <div className="lg:pr-4">
-                            <div className="lg:max-w-lg">
-                                <p className="text-base font-semibold leading-7 text-indigo-600">Full Stack
-                                    Developer</p>
-                                <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Steven
-                                    Rhett Studios
-                                </h1>
-                                <p className="mt-6 text-xl leading-8 text-gray-700">
-                                    As a developer I am passionate about learning the latest software
-                                    innovations.
-                                    At present, I am personally invested in a project that I believe will
-                                    impact many people.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div
-                        className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
-                        <img
-                            className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
-                            src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
-                            alt=""
+        <div className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
+            <div className="absolute inset-0 -z-10 overflow-hidden">
+                <svg
+                    className="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]"
+                    aria-hidden="true"
+                >
+                    <defs>
+                        <pattern
+                            id="e813992c-7d03-4cc4-a2bd-151760b470a0"
+                            width={200}
+                            height={200}
+                            x="50%"
+                            y={-1}
+                            patternUnits="userSpaceOnUse"
+                        >
+                            <path d="M100 200V.5M.5 .5H200" fill="none"/>
+                        </pattern>
+                    </defs>
+                    <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
+                        <path
+                            d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
+                            strokeWidth={0}
                         />
-                    </div>
-                    <main className="bg-gradient-to-r from-indigo-100 to-transparent">
-
-                        <div>
-                            <h2 className="text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">Skills</h2>
+                    </svg>
+                    <rect width="100%" height="100%" strokeWidth={0}
+                          fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)"/>
+                </svg>
+            </div>
+            <div
+                className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
+                <div
+                    className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+                    <div className="lg:pr-4">
+                        <div className="lg:max-w-lg">
+                            <p className="text-base font-semibold leading-7 text-indigo-600">Full Stack
+                                Developer</p>
+                            <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Steven
+                                Rhett Studios
+                            </h1>
                             <p className="mt-6 text-xl leading-8 text-gray-700">
-                                I have a wide range of skills that I am capable of.
+                                As a developer I am passionate about learning the latest software
+                                innovations.
+                                At present, I am personally invested in a project that I believe will
+                                impact many people.
                             </p>
                         </div>
-                        <div className="mt-12">
-                            <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+                    </div>
+                </div>
+                <div
+                    className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
+                    <img
+                        className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
+                        src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+                        alt=""
+                    />
+                </div>
+                <div
+                className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
 
+                <main className="bg-gradient-to-r px-2 py-6 from-indigo-50 to-transparent">
+                    <div className="lg:pr-4">
+                        <div className="lg:max-w-lg">
+                        <h2 className="text-3xl font-bold leading-8 text-center tracking-tight text-gray-900 sm:text-4xl">
+                            <Link to="/projects">Recent Work</Link>
 
-                                <div>
-                                    <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900"></h2>
-                                    <p className="mt-4 text-lg leading-relaxed text-gray-500 sm:text-xl">
-                                        <span className="text-indigo-600"></span>
-                                        <br/>
-                                    </p>
-                                    <div
-                                        className="mb-10 lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
-                                        <div className="p-4 ml-6 lg:pr-4">
-                                            <div className="mt-8 space-y-8 text-gray-600">
-                                                <p>
-                                                </p>
-                                                <p className="mt-8">
-                                                    I am currently working on a project called{" "}
-                                                    <Link to="https://www.felontech.org"
-                                                          className="font-semiBold text-indigo-800">
-                                                        FelonTech
-                                                    </Link>
-                                                    {" "}
-                                                    which is a social platform geared toward returning citizens
-                                                    wishing to learn or refresh on modern technology. It
-                                                    allows users to connect with and learn at a self-paced
-                                                    environment.
-                                                </p>
-                                                <p className="mt-6">
-                                                    I am also working on a project called{" "}
-                                                    <Link to="https://www.pawsome-github-io.vercel.app"
-                                                          className="font-semiBold text-indigo-800">
-                                                        Pawsome
-                                                    </Link>{" "}
-                                                    <span> 🐾🐾 </span>
-                                                    which is a social platform geared toward pet owners and pet
-                                                    trainers.
-                                                </p>
-                                            </div>
-                                            <button
-                                                className="btn-ghost bg-indigo-50 border-blue-500 px-2 py-1 rounded">
-                                                <Link to="/projects">Recent Work</Link>
-                                            </button>
+                        </h2>
+                        <p className="mt-6 text-xl leading-8 text-gray-700">
 
-                                        </div>
-                                    </div>
+                            I am currently working on a project called{" "}
+                            <Link to="https://www.felontech.org"
+                                  className="font-semiBold text-indigo-800">
+                                FelonTech
+                            </Link>
+                            {" "}
+                            which is a social platform geared toward returning citizens
+                            wishing to learn or refresh on modern technology. It
+                            allows users to connect with and learn at a self-paced
+                            environment.
+                        </p>
+                        <p className="mt-6">
+                            I am also working on a project called{" "}
+                            <Link to="https://www.pawsome-github-io.vercel.app"
+                                  className="font-semiBold text-indigo-800">
+                                Pawsome
+                            </Link>{" "}
+                            <span> 🐾🐾 </span>
+                            which is a social platform geared toward pet owners and pet
+                            trainers.
+                        </p>
 
-                                </div>
-                            </dl>
-                        </div>
-                    </main>
+                    </div>
+                    </div>
+
+                </main>
                 </div>
             </div>
-        </div>)
-}
+        </div>
+
+        </div>
+        )
+        }
 
 
 
--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
-import {Dialog, Menu} from '@headlessui/react'
+import {Dialog} from '@headlessui/react'
 import {XMarkIcon} from "@heroicons/react/24/outline";
-
+import {Link, NavLink} from "react-router-dom";
+import {Bars4Icon} from "@heroicons/react/20/solid";
 
 const navigation = [
     { name: "Home", href: "/" },
@@ -15,11 +16,11 @@ const navigation = [
         <header className="bg-base">
             <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
                 <div className="flex lg:flex-1">
-                    <a href="#" className="-m-1.5 p-1.5">
-                        <span className="sr-only">Your Company</span>
-                        <img className="h-8 w-auto" src="/src/assets/Ste_Rhe.svg"
-                             alt="" />
-                    </a>
+                    <Link to={"/"} className="-m-1.5 p-1.5">
+                        <span className="sr-only">Steven Rhett Studios</span>
+                        <img className="hidden lg:flex lg:h-28 w-auto" src="/src/assets/Ste_Rhe.svg"
+                             alt=""/>
+                    </Link>
                 </div>
                 <div className="flex lg:hidden">
                     <button
@@ -28,21 +29,19 @@ const navigation = [
                         onClick={() => setMobileMenuOpen(true)}
                     >
                         <span className="sr-only">Open main menu</span>
-                        <Menu className="h-6 w-6" aria-hidden="true"/>
-
-
+                        <Bars4Icon className="h-8 w-10" aria-hidden="true"/>
                     </button>
                 </div>
                 <div className="hidden lg:flex lg:gap-x-12">
                     {navigation.map((item) => (
-                        <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+                        <NavLink to={item.href} key={item.name} href={item.href}
+                            className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
                             {item.name}
-                        </a>
+                        </NavLink>
                     ))}
                 </div>
                 <div className="hidden lg:flex lg:flex-1 lg:justify-end">
                     <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                        Log in <span aria-hidden="true">&rarr;</span>
                     </a>
                 </div>
             </nav>
@@ -50,14 +49,14 @@ const navigation = [
                 <div className="fixed inset-0 z-10" />
                 <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div className="flex items-center justify-between">
-                        <a href="#" className="-m-1.5 p-1.5">
-                            <span className="sr-only">Your Company</span>
+                        <Link to={"/"} className="-m-1.5 p-1.5">
+                            <span className="sr-only">Steven Rhett Studio</span>
                             <img
                                 className="h-8 w-auto"
                                 src="/src/assets/Ste_Rhe.svg"
                                 alt=""
                             />
-                        </a>
+                        </Link>
                         <button
                             type="button"
                             className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -73,23 +72,18 @@ const navigation = [
                         <div className="-my-6 divide-y divide-gray-500/10">
                             <div className="space-y-2 py-6">
                                 {navigation.map((item) => (
-                                    <a
+                                    <NavLink to={item.href}
                                         key={item.name}
                                         href={item.href}
                                         className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                                     >
                                         {item.name}
-                                    </a>
+                                    </NavLink>
                                 ))}
                             </div>
-                            <div className="py-6">
-                                <a
-                                    href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                                >
-                                    Log in
-                                </a>
-                            </div>
+                            {/*<div className="py-6">*/}
+
+                            {/*</div>*/}
                         </div>
                     </div>
                 </Dialog.Panel>
