71515a08604366aca9947e26bbab2b2a0b9ce227
stevenrhett <johnsonsteven18@gmail.com>
2023-07-30 21:46:13 -0400

    new

--- src/Body.jsx
+++ src/Body.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Body() {
     return (
-        <div className="bg-white">
+        <div className="bg-gray-100">
             <main>
                 {/* Hero section */}
                 <div className="relative isolate overflow-hidden">
@@ -16,7 +16,7 @@ export default function Body() {
                                 <path d="M.5 200V.5H200" fill="none"/>
                             </pattern>
                         </defs>
-                        <svg x="50%" y={-1} className="overflow-visible fill-gray-800/20">
+                        <svg x="50%" y={-1} className="overflow-visible fill-gray-400/20">
                             <path
                                 d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
                                 strokeWidth={0}/>
@@ -71,7 +71,7 @@ export default function Body() {
                                 I am currently working on a project called{" "}
                                 <a href="https://www.felontech.org" target="_blank" rel="noopener noreferrer"
                                    className="text-indigo-600 hover:text-indigo-500">
-    FelonTech
+    FelonTech{" "}
 
 
 
--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -13,7 +13,7 @@ const navigation = [
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
     return (
-        <header className="bg-base">
+        <header className="bg-gray-100">
             <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
                 <div className="flex lg:flex-1">
                     <Link to={"/"} className="-m-1.5 p-1.5">
