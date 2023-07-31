66b906cff73aaac34cfdba58451aed630182164f
stevenrhett <johnsonsteven18@gmail.com>
2023-07-31 15:13:30 -0400

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,6 @@ module.exports = {
     },
     plugins: [
         require('@tailwindcss/forms'),
-        require('@tailwindcss/line-clamp'),
         require('@tailwindcss/typography'),
         require('@tailwindcss/aspect-ratio'),
         require("daisyui")
