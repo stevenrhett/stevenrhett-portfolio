c1d748567965a00b41faf39e63024cb095a0bba4
stevenrhett <johnsonsteven18@gmail.com>
2023-07-31 14:51:10 -0400

    new

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,13 +5,25 @@ module.exports = {
         "./src/**/*.{js,ts,jsx,tsx}",
 
     ],
-    theme: {},
+    theme: {
+        screens: {
+            'sm': '576px',
+            // => @media (min-width: 767px) { ... }
+            'md': '768px',
+            // => @media (min-width: 768px) { ... }
+            'lg': '1024px',
+            // => @media (min-width: 1024px) { ... }
+            'xl': '1600px',
+            // => @media (min-width: 1600px) { ... }
+        },
+    },
 
-    plugins: [require("@headlessui/tailwindcss"),
+    plugins: [
         require("@tailwindcss/forms"),
         require("@tailwindcss/typography"),
-        require("daisyui"),
         require('@tailwindcss/aspect-ratio'),
+        require("@headlessui/tailwindcss"),
+        require("daisyui"),
     ],
 
 };
